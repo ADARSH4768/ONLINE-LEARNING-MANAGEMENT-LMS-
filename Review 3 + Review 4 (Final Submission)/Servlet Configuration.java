4bcccaf95d13af1d89ac48c14ec0818bcf2a46ad
Adarsh Tripathi <tripathiadarsh4768@gmail.com>
2025-01-04 19:16:56 +0530

    Delete Review 3 + Review 4 (Final Submission) directory

--- Review 3 + Review 4 (Final Submission)/Servlet Configuration.java
+++
@@ -1,8 +0,0 @@
-@WebServlet("/user")
-public class UserServlet extends HttpServlet {
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        System.out.println("UserServlet Initialized");
-    }
-}
--- Review 3 + Review 4 (Final Submission)/Unit Tests for Service & DAO.java
+++
@@ -1,7 +0,0 @@
-@Test
-public void testSaveUser() {
-    UserDAO userDAO = new UserDAO();
-    User user = new User("TestUser", "test@example.com");
-    boolean result = userDAO.save(user);
-    assertTrue(result);
-}
